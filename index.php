3834fcc1eb0448a91d084ba3fbf924a84e8ec078
Zabdipe <140554557+Zabdipe@users.noreply.github.com>
2023-08-31 18:54:18 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <met http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <tittle></tittle>
+    <link rel="stylesheet" hret="style.css">
+</head>
+<body>
+
+    <form method="post" >
+
+     <h2>Formulario</h2>
+     <p>Inicia tu registro</p>
+
+     <div class="input-wrapper">
+        <input type="text" name="name" placeholder="Nombre" >
+        <img class="input-icon" src="images/name.svg.jpg" alt="">
+     </div>
+     <div class="input-wrapper">
+        <input type="email" name="email" placeholder="Email" >
+        <img class="input-icon" src=images/correo.svg.jpg alt="">
+     </div>
+     <div class="input-wrapper">
+        <input type="text" name="direccion" placeholder="Direccion" >
+        <img class="input-icon" src=images/direccion.svg.jpg alt="">
+     </div>
+     <div class="input-wrapper">
+        <input type="tel" name="phone" placeholder="Telefono" >
+        <img class="input-icon" src="images/Telefono.svg.jpg" alt="">
+     </div>
+     <div class="input-wrapper">
+        <input type="password" name="password" placeholder="Contraseña" >
+        <img class="input-icon" src=images/contraseña.svg.jpg alt="">
+     </div>
+     <input class="btn" type="submit" name="register" value="Enviar">
+</form>
+
+</body>
+</html>
